8886638f1afa090b6154a921218d9eef7c29f5e6
Hely <skljhly.666@gmail.com>
2019-10-15 22:38:42 -0600

    created customer migration

---
+++ database/migrations/2019_10_16_001103_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('code',50)->unique();
+            $table->string('fullname',60);
+            $table->string('cui',15);
+            $table->string('number_phone',12);
+            $table->string('description',100)->nullable();
+            $table->boolean('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
