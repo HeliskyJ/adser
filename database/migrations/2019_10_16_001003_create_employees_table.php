58b5785f7dfd1a3dbbd33751765af7ef831d1deb
Hely <skljhly.666@gmail.com>
2019-10-15 22:38:05 -0600

    Created employee migration

---
+++ database/migrations/2019_10_16_001003_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('code',50)->unique();
+            $table->string('fullname',60);
+            $table->string('cui',15);
+            $table->string('number_phone',12);
+            $table->string('description',100)->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->boolean('busy_time')->default(0);
+            $table->boolean('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
