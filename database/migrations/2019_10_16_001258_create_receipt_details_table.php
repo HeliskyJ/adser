13e04fe351070bc5c7428a9a23cb2ccc3cbbd205
Hely <skljhly.666@gmail.com>
2019-10-15 22:42:26 -0600

    Created receipt_details migration

---
+++ database/migrations/2019_10_16_001258_create_receipt_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceiptDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipt_details', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->unsignedBigInteger('receipts_id');
+            $table->unsignedBigInteger('services_id');
+            $table->tinyInteger('flat')->unsigned();
+            $table->decimal('price',10,3)->unsigned();
+            $table->decimal('total',10,4)->unsigned();
+            $table->string('address',60);
+            $table->date('date_service');
+            $table->time('service_time');
+            $table->string('description', 60)->nullable();
+            $table->timestamps();
+
+            $table->foreign('receipts_id')->references('id')->on('receipts');
+            $table->foreign('services_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipt_details');
+    }
+}
