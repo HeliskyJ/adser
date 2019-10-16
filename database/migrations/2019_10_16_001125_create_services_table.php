e9e01d8412c7fc119c37424d47ffd7407a5953ec
Hely <skljhly.666@gmail.com>
2019-10-15 22:40:39 -0600

    Created services migration

---
+++ database/migrations/2019_10_16_001125_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('code',50)->unique();
+            $table->string('name',60);
+            $table->decimal('price',10,3)->unsigned();
+            $table->string('description',100)->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->boolean('done')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
