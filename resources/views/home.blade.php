a70ffd937dd0d7003faa71911cd36aa9601cb168
Hely <skljhly.666@gmail.com>
2019-11-22 21:58:50 -0600

    Added design to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,120 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+<div class="column is-four-fifths">
+    <div class="container">
+        <div class="column is-2"></div><br><br>
+<section class="columns is-multiline">
+    <div class="column">
+        <div class="box notification">
+                @if ($record == 1)
+                <span class="heading has-text-centered"> Servicio Prestado</span>
+                @else
+                <span class="heading has-text-centered">Servicios Prestados</span>
+                @endif
+            <span class="title">{{ $record }}</span>
+            <div class="level">
+                <div class="level-item">
+                    <div>
+                        <div class="heading">Porcentaje</div>
+                        <div class="title is-5">45%</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                      <div>
+                        <div class="heading">Porcentaje</div>
+                        <div class="title is-5">45%</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div>
+                        <div class="heading">Porcentaje</div>
+                        <div class="title is-5">45%</div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="column">
+            <div class="box notification">
+                    @if ($pending == 1)
+                    <span class="heading has-text-centered"> Servicio Pendiente</span>
+                    @else
+                    <span class="heading has-text-centered">Servicios Pendientes</span>
+                    @endif
+                <span class="title">{{ $pending }}</span>
+                <div class="level">
+                    <div class="level-item">
+                        <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                          <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                        <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="column">
+            <div class="box notification">
+                    @if ($cancel == 1)
+                    <span class="heading has-text-centered"> Servicio Cancelado</span>
+                    @else
+                    <span class="heading has-text-centered">Servicios Cancelados</span>
+                    @endif
+                <span class="title">{{ $cancel }}</span>
+                <div class="level">
+                    <div class="level-item">
+                        <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                          <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                    <div class="level-item">
+                        <div>
+                            <div class="heading">Porcentaje</div>
+                            <div class="title is-5">45%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+<section class="columns is-multiline"><br>
+    <div class="column is-6">
+        <article class="message is-dark">
+            <div class="message-header">Servicios</div>
+            <div class="message-body" style="position:relative;">
+                    <div>Hoa</div>
+                    </div>
+        </article>
+    </div>
+    <div class="column is-6">
+        <article class="message is-dark">
+            <div class="message-header">Empleados Activos</div>
+            <div class="message-body" style="position:relative;">
+                    <div>Hoa</div>
+                    </div>
+        </article>
+    </div>
+</section>
+</div>
+</div>
 </div>
 @endsection
