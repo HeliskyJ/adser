fdb51ed1978416980f42d917313bac0c506b1fdb
Hely <skljhly.666@gmail.com>
2019-11-22 23:04:31 -0600

    Modified design aside navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,24 +2,34 @@
     {{-- <div class="column is-1" id="bodyNav"></div> --}}
     <div class="column is-2 has-background-grey-dark">
             <aside  class="menu"><br>
+              <nav class="menu">
                     <p class="menu-label has-text-light">
                       General
                     </p>
                     <ul class="menu-list">
-                      <li><a class="has-text-light">Inicio</a></li>
+                      <li><a href="{{ route('home') }}" class="is-active has-text-light">
+                        <span class="icon is-small">
+                          <i class="fa fa-tachometer"></i>
+                        </span>
+                        Inicio</a></li>
                     </ul>
                     <p class="menu-label has-text-light">
                       Administration
                     </p>
                     <ul class="menu-list">
-                      <li>
                         <ul>
-                          <li><a class="has-text-light fas fa-hands-helping" href="{{ route('service.index') }}"> Servicios</a></li>
-                          <li><a class="has-text-light fa fa-hourglass" href="#"> Horarios</a></li>
-                          <li><a class="has-text-light fa fa-users" href="{{ route('employee.index') }}"> Empleados</a></li>
-                          <li><a class="has-text-light fas fa-street-view" href="{{ route('customer.index') }}" > Clientes</a></li>
+                          <li><a class="has-text-light" href="{{ route('service.index') }}">
+                            <span class="icon is-small">
+                              <i class=" fas fa-hands-helping"></i></span>
+                              <font style="vertical-align: inherit;">Servicios</font>
+                          </a></li>
+                          <li><a class="has-text-light" href="{{ route('employee.index') }}"> 
+                            <i class=" fa fa-users" ></i> Empleados</a></li>
+                          <li><a class="has-text-light" href="{{ route('customer.index') }}" >
+                            <i class="fas fa-street-view"></i> Clientes</a></li>
+                          <li><a class="has-text-light " href="{{ route('receipt.create') }}">
+                            <i class="fa fa-calendar-alt"></i> Agendar Servicio</a></li>
                         </ul>
-                      </li>
                     </ul>
                     <p class="menu-label has-text-light">
                       Configurar
@@ -29,5 +39,6 @@
                       <li><a class="has-text-light">Transfers</a></li>
                       <li><a class="has-text-light">Balance</a></li>
                     </ul>
+                  </nav>
                   </aside><br><br>
     </div>
