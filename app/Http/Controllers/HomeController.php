a6a29746de1219e5530309d00a1fcfe229f3d8b3
Hely <skljhly.666@gmail.com>
2019-11-24 21:15:23 -0600

    Customer design view home and added queries in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request,
-App\Receipt;
+App\Receipt,
+App\Employee,
+App\Customer;
 
 class HomeController extends Controller
 {
@@ -24,9 +26,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $date    = date('Y-m-d');
+        $month   = date('m');
+        $now     = Receipt::all()->where('is_active', 1)->where('done', 0)->where('date_service', $date);
+        $month   = Receipt::all()->where('is_active', 1);
+        $etotal  = Employee::all()->where('deleted', 0)->where('is_active', 1)->count();
+        $ctotal  = Customer::all()->where('deleted', 0)->count();
         $record  = Receipt::all()->where('is_active', 1)->where('done', 1)->count();
         $pending = Receipt::all()->where('is_active', 1)->where('done', 0)->count();
         $cancel  = Receipt::all()->where('is_active', 0)->where('done', 0)->count();
-        return view('home', compact('record', 'pending', 'cancel'));
+        return view('home', compact('record', 'pending', 'cancel', 'now', 'etotal', 'ctotal'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="column is-four-fifths">
 <section class="hero is-fullheight letter">
-    <div class="hero-body">
-    </section>
-    <div class="column is-four-fifths">
-        <div class="container">
-            <div class="column is-2"></div><br><br>
-    <section class="columns is-multiline">
-        <div class="column">
-            <div class="box notification">
-                    @if ($record == 1)
-                    <span class="heading has-text-centered"> Servicio Prestado</span>
-                    @else
-                    <span class="heading has-text-centered">Servicios Prestados</span>
-                    @endif
-                <span class="title">{{ $record }}</span>
-                <div class="level">
-                    <div class="level-item">
-                        <div>
-                            <div class="heading">Porcentaje</div>
-                            <div class="title is-5">1%</div>
-                        </div>
-                    </div>
-                    <div class="level-item">
-                          <div>
-                            <div class="heading">Porcentaje</div>
-                            <div class="title is-5">45%</div>
-                        </div>
-                    </div>
-                    <div class="level-item">
-                        <div>
-                            <div class="heading">Porcentaje</div>
-                            <div class="title is-5">45%</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-                <div class="box notification">
-                        @if ($pending == 1)
-                        <span class="heading has-text-centered"> Servicio Pendiente</span>
+    <div class="hero-head"><br>
+            <div class="container">
+        <section class="columns is-multiline">
+            <div class="column">
+                <div class="box notification is-warning">
+                        @if ($record == 1)
+                        <span class="heading has-text-centered"> Servicio Prestado</span>
                         @else
-                        <span class="heading has-text-centered">Servicios Pendientes</span>
+                        <span class="heading has-text-centered">Servicios Prestados</span>
                         @endif
-                    <span class="title">{{ $pending }}</span>
+                    <span class="title">{{ $record }}</span>
                     <div class="level">
                         <div class="level-item">
                             <div>
                                 <div class="heading">Porcentaje</div>
-                                <div class="title is-5">45%</div>
+                                <div class="title is-5">1%</div>
                             </div>
                         </div>
                         <div class="level-item">
@@ -68,58 +36,177 @@
                     </div>
                 </div>
             </div>
-        <div class="column">
-                <div class="box notification">
-                        @if ($cancel == 1)
-                        <span class="heading has-text-centered"> Servicio Cancelado</span>
-                        @else
-                        <span class="heading has-text-centered">Servicios Cancelados</span>
-                        @endif
-                    <span class="title">{{ $cancel }}</span>
-                    <div class="level">
-                        <div class="level-item">
-                            <div>
-                                <div class="heading">Porcentaje</div>
-                                <div class="title is-5">45%</div>
+            <div class="column">
+                    <div class="box notification is-primary">
+                            @if ($pending == 1)
+                            <span class="heading has-text-centered"> Servicio Pendiente</span>
+                            @else
+                            <span class="heading has-text-centered">Servicios Pendientes</span>
+                            @endif
+                        <span class="title">{{ $pending }}</span>
+                        <div class="level">
+                            <div class="level-item">
+                                <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="level-item">
-                              <div>
-                                <div class="heading">Porcentaje</div>
-                                <div class="title is-5">45%</div>
+                            <div class="level-item">
+                                  <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="level-item">
-                            <div>
-                                <div class="heading">Porcentaje</div>
-                                <div class="title is-5">45%</div>
+                            <div class="level-item">
+                                <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            <div class="column">
+                    <div class="box notification" id="backBox">
+                            @if ($cancel == 1)
+                            <span class="heading has-text-centered"> Servicio Cancelado</span>
+                            @else
+                            <span class="heading has-text-centered">Servicios Cancelados</span>
+                            @endif
+                        <span class="title">{{ $cancel }}</span>
+                        <div class="level">
+                            <div class="level-item">
+                                <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
+                            </div>
+                            <div class="level-item">
+                                  <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
+                            </div>
+                            <div class="level-item">
+                                <div>
+                                    <div class="heading">Porcentaje</div>
+                                    <div class="title is-5">45%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section><br>
+        <section class="columns is-multiline"><br>
+            <div class="column is-9">
+                <article class="message is-dark">
+                    <div class="message-header">SERVICIOS PARA HOY</div>
+                    <div class="message-body ScrollStyle" style="position:relative;">
+                        <table class="table is-fullwidth is-striped is-hoverable">
+                            <thead>
+                                <th>Hora Inicio</th>
+                                <th>Hora Fin</th>
+                                <th>Empleado</th>
+                                <th>Dirección</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($now as $item)
+                                    <tr>
+                                        <td>{{ $item->schedule->hour }}</td>
+                                        <td>{{ $item->service_end }}</td>
+                                        <td>{{ $item->employee->fullname }}</td>
+                                        <td>{{ $item->address }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                    </table>
+                            </div>
+                </article>
             </div>
-    </section>
-    <section class="columns is-multiline"><br>
-        <div class="column is-6">
-            <article class="message is-dark">
-                <div class="message-header">Servicios</div>
-                <div class="message-body" style="position:relative;">
-                        <div>Hoa</div>
+            <div class="column is-3">
+                <article class="message is-dark">
+                    <div class="message-header"></div>
+                    <div class="message-body" style="position:relative;">
+                        <div class="box">
+                            <article class="media">
+                                <div class="media-left">
+                                    <figure class="image is-64x64">
+                                        <img src="{{ asset('images/today.png') }}" width="80" alt="MES">
+                                    </figure>
+                                </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <small>En </small><strong class="is-size-5">Noviembre</strong>
+                                        <br>
+                                        15 Servicios
+                                    </p>
+                                </div>
+                            </div>
+                            </article>
                         </div>
+                        <div class="box">
+                            <article class="media">
+                                <div class="media-left">
+                                    <figure class="image is-64x64">
+                                        <img src="{{ asset('images/year.png') }}" width="80" alt="AÑO">
+                                    </figure>
+                                </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <small>En </small><strong class="is-size-5">2019</strong>
+                                        <br>
+                                       13 Servicios 
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img src="{{ asset('images/client.png') }}" width="80"  alt="CLIENTES">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong class="is-size-5">Clientes</strong>
+                                        <br>
+                                        {{ $ctotal }}  
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img src="{{ asset('images/male.png') }}" width="80" alt="COLABORADORES">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong class="is-size-5">Empleados</strong>
+                                        <br>
+                                        {{ $etotal }}
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
             </article>
         </div>
-        <div class="column is-6">
-            <article class="message is-dark">
-                <div class="message-header">Empleados Activos</div>
-                <div class="message-body" style="position:relative;">
-                        <div>Hoa</div>
-                        </div>
-            </article>
-        </div>
     </section>
-    </div>
-    </div>
-
+ </div>
+</section>
+    
+</div>
     </div>
 </div>
 @endsection
