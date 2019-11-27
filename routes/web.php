06c63e652e2a0a90cca4f0e189ececedf37d800e
Helisky <skhely998@gmail.com>
2019-11-27 08:07:52 -0600

    style css

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,24 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ReceiptController;
 use App\Http\Controllers\ServiceController;
 use App\Receipt;
-
+/**
+ * @description Show the welcome page of the site
+ */
 Route::get('/', function () {
     return view('welcome');
 });
-
+/**
+ * @description Show the view auth of the site
+ */
 Auth::routes();
-
+/**
+ * @description Show the home page of the site
+ */
 Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * @description customer of the site
+ */
 Route::get('/customer', 'CustomerController@create')->name('customer.create');
 Route::post('/customer', 'CustomerController@store')->name('customer.store');
 Route::get('/customers', 'CustomerController@index')->name('customer.index');
@@ -31,6 +41,9 @@ Route::get('/customer/{cut}/edit', 'CustomerController@edit')->name('customer.ed
 Route::patch('/customers/{cust}', 'CustomerController@update')->name('customer.update')->middleware('password.confirm');
 Route::patch('/customer/delete/{cust}', 'CustomerController@destroy')->name('customer.delete')->middleware('password.confirm');
 
+/**
+ * @description employee of the site
+ */
 Route::get('/employees', 'EmployeeController@index')->name('employee.index');
 Route::get('/employee', 'EmployeeController@create')->name('employee.create');
 Route::post('/employee', 'EmployeeController@store')->name('employee.store');
@@ -41,6 +54,9 @@ Route::patch('employee/delete/{emp}', 'EmployeeController@destroy')->name('emplo
 Route::get('employee/record/{id}', 'EmployeeController@receipts')->name('employee.records');
 Route::get('employee/pending/{id}', 'EmployeeController@pendingServices')->name('employee.pending');
 
+/**
+ * @description service of the site
+ */
 Route::get('/services', 'ServiceController@index')->name('service.index');
 Route::get('/service', 'ServiceController@create')->name('service.create');
 Route::post('/service', 'ServiceController@store')->name('service.store');
@@ -49,6 +65,9 @@ Route::get('/service/{serv}/edit', 'ServiceController@edit')->name('service.edit
 Route::patch('/service/{serv}', 'ServiceController@update')->name('service.update')->middleware('password.confirm');
 Route::patch('/service/delete/{serv}', 'ServiceController@destroy')->name('service.delete')->middleware('password.confirm');
 
+/**
+ * @description Receipt of the site
+ */
 Route::get('/receipt/findCustomer', 'ReceiptController@findCustomer');
 Route::get('/receipt/findService', 'ReceiptController@findService');
 Route::get('/receipt/findEmployee', 'ReceiptController@findEmployee');
